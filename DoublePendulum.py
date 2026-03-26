63a474738b3503ac65cfe714a46e46f089c764ee
maddie.moffett <mnrmoffett@gmail.com>
2026-03-26 16:41:56 -0600

    fix omega naming; add part A

--- DoublePendulum.py
+++ DoublePendulum.py
@@ -10,20 +10,26 @@ from math import cos, sin
 # x2 = l*sin(theta1) + l*sin(theta2)  y2 = -l*cos(theta1) - l*cos(theta2)
 
 # Potential Energy
-# V = m*g*y1 + m*g*y2 = -m*g*l*(2*cos(theta1) + cos(theta2))
+# V = m*g*y1 + m*g*y2 
+# = -m*g*l*(2*cos(theta1) + cos(theta2))
 
 # VELOCITIES
-# dx1 = l * dtheta1 * cos(theta1)
-# dy1 = l * dtheta1* sin(theta1)
-# dx2 = l * dtheta1 * cos(theta1) + l * dtheta2 * cos(theta2)
-# dy2 = l * dtheta1* sin(theta1) + l * dtheta2* sin(theta2)
+# dx1 = l * omega1 * cos(theta1)
+# dy1 = l * omega1* sin(theta1)
+# dx2 = l * omega1 * cos(theta1) + l * omega2 * cos(theta2)
+# dy2 = l * omega1* sin(theta1) + l * omega2* sin(theta2)
 
 # Kinetic Energy
 # T = (1/2) * m * (dx1**2 + dy1**2) + (1/2) * m * (dx2**2 + dy2**2)
-# T = (1/2) * m * (l**2) * (2 * detheta1**2 + dtheta2**2 + 2*dtheta1*dtheta2(cos(theta1) * cos(theta2) + sin(theta1) * sin(theta2)))
-# T = (1/2) * m * (l**2) * (2*(dtheta1**2) + dtheta2**2 + 2*dtheta1*dtheta2*cos(theta1-theta2))
+# T = (1/2) * m * (l**2) * (2 * detheta1**2 + omega2**2 + 2*omega1*omega2(cos(theta1) * cos(theta2) + sin(theta1) * sin(theta2)))
+# T = (1/2) * m * (l**2) * (2*(omega1**2) + omega2**2 + 2*omega1*omega2*cos(theta1-theta2))
 
 # EQUATIONS OF MOTION
-# dtheta1 = omega1 dtheta2 = omega2
 # domega1 = -(omega1**2 *sin(2*theta1 - 2*theta2) + 2 *omega2**2 *sin(theta1 - theta2) + (g/l)*(sin(theta1- 2*theta2) + 3*sin(theta1)))) / (3-cos(2*theta1 - 2*theta2))
 # domega2 = (4*omega1**2 *sin(theta1 - theta2) + omega2**2 *sin(2*theta1 - 2*theta2) + (g/l)*(sin(2*theta1- theta2) - sin(theta2)))) / (3-cos(2*theta1 - 2*theta2))
+
+def PartA(theta1, theta2, omega1, omega2, m, g, l):
+    V = -m*g*l*(2*cos(theta1) + cos(theta2))
+    T = (1/2)*m*(l**2) * (2*(omega1**2) + omega2**2 + 2*omega1*omega2*cos(theta1-theta2))
+    E = V + T
+    return E
